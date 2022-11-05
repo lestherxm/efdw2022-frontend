400dbff4913caf8bea82f0d6153b3c273e8999c0
lestherxm <lestherxm@gmail.com>
2022-11-05 03:08:53 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'efdw2022-frontend';
+  // gestion de sidenav para responsive web design
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  constructor(private oberver: BreakpointObserver){
+
+  }
+
+  ngAfterViewInit(){
+    this.oberver.observe(['(max-width: 800px)']).subscribe((res) =>{
+      if(res.matches){
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      }else{
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
+
+
 }
